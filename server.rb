a54d563e13c63afa5a0393ce425d66c7db8f53e9
Polo Li <pololi@stripe.com>
2021-04-07 21:44:46 -0700

    pl - change it to use my BR scenarios keys

--- server.rb
+++ server.rb
@@ -1,8 +1,10 @@
+# frozen_string_literal: true
+
 require 'stripe'
 require 'sinatra'
 
 # This is a sample test API key. Sign in to see examples pre-filled with your key.
-Stripe.api_key = 'sk_test_4eC39HqLyjWDarjtT1zdp7dc'
+Stripe.api_key = 'sk_test_51IXxRcC6H73589PKL8IF5CdUUqmhaMFbVthnhyriVftkRyVtEeSDwIc8fRTMNrbPAsXhsK1jEHZlJemM2Wh9dME200Uhgka7rN'
 
 set :static, true
 set :public_folder, File.dirname(__FILE__)
@@ -13,12 +15,12 @@ YOUR_DOMAIN = 'http://localhost:4242'
 post '/create-checkout-session' do
   content_type 'application/json'
 
-  session = Stripe::Checkout::Session.create({
-    payment_method_types: ['card'],
+  session = Stripe::Checkout::Session.create(
+    payment_method_types: ['card', 'boleto'],
     line_items: [{
       price_data: {
         unit_amount: 2000,
-        currency: 'usd',
+        currency: 'brl',
         product_data: {
           name: 'Stubborn Attachments',
           images: ['https://i.imgur.com/EHyR2nP.png'],
@@ -29,7 +31,7 @@ post '/create-checkout-session' do
     mode: 'payment',
     success_url: YOUR_DOMAIN + '/success.html',
     cancel_url: YOUR_DOMAIN + '/cancel.html',
-  })
+  )
 
   {
     id: session.id
